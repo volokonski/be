83790de5d3deae631dbe61a292af8926eb38b93f
Anton Volokonski <anton.volo@gmail.com>
2023-09-07 13:30:33 +0300

    add sobaki greetings

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-const http = require('http')
-const port = process.env.PORT || 3000
+const http = require("http");
+const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Yo, sobaki!')
-})
-server.listen(port, () => console.log(`сервер запущен на порте ${port}; ` +
-    'нажмите Ctrl+C для завершения...'))
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Yo, sobaki! :)");
+});
+server.listen(port, () =>
+  console.log(
+    `сервер запущен на порте ${port}; ` + "нажмите Ctrl+C для завершения..."
+  )
+);
