04b24d3927ee3f5a33b0fb7d6a6246aee641fc96
Anton Volokonski <anton.volo@gmail.com>
2023-09-07 13:47:55 +0300

    add nodemon comment

--- test.js
+++ test.js
@@ -2,10 +2,12 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Yo, sobaki! :)");
+  res.end("Yo, sobaki! :-)");
 });
 server.listen(port, () =>
   console.log(
-    `сервер запущен на порте ${port}; ` + "нажмите Ctrl+C для завершения..."
-  )
+    `сервер запущен на порте ${port}; ` + "нажмите Ctrl+C для завершения...",
+  ),
 );
+
+// nodemon test.js
